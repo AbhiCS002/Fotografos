24d1dbf42f819a06738b3383ea55fc9dc0dc00d5
Abhilash S <abhilashs@saumya-mac-mini.local>
2020-02-19 15:10:22 +0530

    Initial Commit

---
+++ Fotografos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fotografos
+//
+//  Created by Abhilash S (BLR GSS) on 19/02/20.
+//  Copyright © 2020 Abhilash S (BLR GSS). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
